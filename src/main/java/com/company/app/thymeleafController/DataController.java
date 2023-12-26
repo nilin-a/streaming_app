6d43f4734cd4ce90775e89745b8c5f12a22a01fe
Elizabeth Doonily <liza.ry-samara@mail.ru>
2023-12-26 17:48:54 +0400

    smth was changed

--- src/main/java/com/company/app/thymeleafController/DataController.java
+++ src/main/java/com/company/app/thymeleafController/DataController.java
@@ -4,9 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@Controller
+@Controller("/thyme")
 public class DataController {
-    @GetMapping
+    @GetMapping(value = "/index")
     public String index(Model model) {
         model.addAttribute("smt", "message!");
         return "index";
