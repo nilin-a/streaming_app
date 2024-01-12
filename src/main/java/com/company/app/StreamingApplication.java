89f082c47da37b41b03c284fb4d3c343b344a89c
Elizabeth Doonily <liza.ry-samara@mail.ru>
2024-01-12 08:59:13 +0400

    swagger does not work

--- src/main/java/com/company/app/StreamingApplication.java
+++ src/main/java/com/company/app/StreamingApplication.java
@@ -3,6 +3,8 @@ package com.company.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.core.SpringVersion;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 
 @SpringBootApplication
 public class StreamingApplication {
