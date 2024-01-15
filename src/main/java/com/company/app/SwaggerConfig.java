03ccc972279c63b1f1a361b5fc3b00853a2aea9c
Elizabeth Doonily <liza.ry-samara@mail.ru>
2024-01-15 11:06:53 +0400

    song module was added

--- src/main/java/com/company/app/SwaggerConfig.java
+++ src/main/java/com/company/app/SwaggerConfig.java
@@ -10,23 +10,12 @@ import springfox.documentation.spring.web.plugins.Docket;
 
 @Configuration
 public class SwaggerConfig {
-    public static final String CITY_TAG = "City Service";
-    public static final String STATE_TAG = "State Service";
-    public static final String COUNTRY_TAG = "Country Service";
-    public static final String GENRE_TAG = "Genre Service";
-    public static final String PERFORMER_TAG = "Performer Service";
-
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
-                .build()
-                /*.tags(new Tag(CITY_TAG, "Service for managing cities"),
-                        new Tag(STATE_TAG, "Service for managing states"),
-                        new Tag(COUNTRY_TAG, "Service for managing countries"),
-                        new Tag(GENRE_TAG, "Service for managing genres"),
-                        new Tag(PERFORMER_TAG, "Service for managing performers"))*/;
+                .build();
     }
 }
