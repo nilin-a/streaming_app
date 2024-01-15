17f91a55c5173d75f49795b4f9f8403484584dcc
Elizabeth Doonily <liza.ry-samara@mail.ru>
2024-01-15 11:09:55 +0400

    performer modul was added

---
+++ performer/src/test/java/com/simbirsoft/performer/PerformerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simbirsoft.performer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerformerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
