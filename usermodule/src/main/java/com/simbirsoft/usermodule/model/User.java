966c1f488c08afe43f9364bc43ed5a2d02d368b0
Elizabeth Doonily <liza.ry-samara@mail.ru>
2024-01-22 10:51:16 +0400

    user modul was added

---
+++ usermodule/src/main/java/com/simbirsoft/usermodule/model/User.java
@@ -0,0 +1,13 @@
+package com.simbirsoft.usermodule.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "app_user")
+public class User {
+}
