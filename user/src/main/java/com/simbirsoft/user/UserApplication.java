d4411c9246a10192af3ed3261bdca012db08511d
Elizabeth Doonily <liza.ry-samara@mail.ru>
2024-01-22 11:00:12 +0400

    user was added

---
+++ user/src/main/java/com/simbirsoft/user/UserApplication.java
@@ -0,0 +1,13 @@
+package com.simbirsoft.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserApplication.class, args);
+    }
+
+}
